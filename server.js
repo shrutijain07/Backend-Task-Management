28a269756db797d24bf4cceab71bf1d4bffe4916
Shruti Jain <jainshruti1479@gmail.com>
2025-01-02 21:27:05 +0530

    commit

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ app.use("/api", taskRoutes);
 // MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log("Connected to MongoDB"))
