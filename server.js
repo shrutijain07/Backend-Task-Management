655a9c49a34d6d75b5b09a588f27ba88160a3e08
Shruti Jain <jainshruti1479@gmail.com>
2025-01-03 06:40:44 +0530

    add some configuration

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ app.use("/api", taskRoutes);
 // MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
     useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
   })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
